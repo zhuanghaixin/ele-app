6773c493a2704a7b5deebedb874e860b07fc42b7
zhuanghaixin <649308293@qq.com>
2020-05-08 20:44:02 +0800

    feat: 构建登录组件

--- src/router/index.js
+++ src/router/index.js
@@ -28,9 +28,18 @@ const router = new VueRouter({
 })
 
 // 路由守卫
-//  router.beforeEach(to,from,next){
-//
-//  }
+ router.beforeEach((to,from,next)=> {
+     const isLogin = localStorage.ele_login ? true : false
+     if (to.path=='/login') {
+        next()
+     }else{
+         //是否在登录状态下
+         isLogin?next():next("/login")
+     }
+ }
+ )
+
+
 
 
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="login">Login
+    <div class="login">
+        <div class="logo">
+            <img src="../assets/logo.jpg" alt="my login image">
+        </div>
 
     </div>
 </template>
@@ -12,13 +15,20 @@
 </script>
 
 <style scoped>
-.login{
-    width: 100%;
-    height: 100%;
-    padding:30px;
-    box-sizing:border-box;
-    background:#fff;
-    text-align: center;
+    .login {
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+        box-sizing: border-box;
+        background: #fff;
+        text-align: center;
 
-}
+    }
+
+    .logo {
+        text-align: center;
+    }
+    .logo img{
+        width:150px;
+    }
 </style>
